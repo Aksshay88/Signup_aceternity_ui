214c5a70f3c49484afacdb9b3ea5e508eb931a3c
Aksshay88 <aksshaybala8846@gmail.com>
2024-05-14 18:14:28 +0530

    Initiated_sign_up_form

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { SignupFormDemo } from "./components/signform";
+
+function App() {
+  return (
+    <section>
+      <SignupFormDemo />
+    </section>
+  );
+}
+
+export default App;
